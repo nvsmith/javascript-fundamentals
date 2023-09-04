c5b3b5f608208ca34f9899118df21ff8033d5983
Nathan Smith <nathanielvs@gmail.com>
2023-09-03 22:21:35 -0600

    time conditionals with Date method

---
+++ module-03/what-time.js
@@ -0,0 +1,20 @@
+// Step 6 of 7
+// What time is it?
+
+var time = new Date().getHours();
+// console.log(time);
+
+var greeting = "Hi";
+var yourName = "Nate";
+
+if (time > 6 && time < 12) {
+    greeting = "Good morning";
+} else if (time < 18) {
+    greeting = "Good afternoon";
+} else if (time < 20) {
+    greeting = "Good evening";
+} else {
+    greeting = "Go to bed";
+}
+
+alert(`${greeting}, ${yourName}!`);
