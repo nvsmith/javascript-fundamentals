25f116b977a400e105d3d8906e6a18722d1c7eab
Nathan Smith <nathanielvs@gmail.com>
2023-09-08 12:23:17 -0600

    add/remove show & hide functionality to image

--- module-05/click-event-kitty/js/script.js
+++ module-05/click-event-kitty/js/script.js
@@ -1,12 +1,20 @@
-// Step 4 of 6
-// Click Event Kitty - program a button to show a cute kitty photo when pressed!
+// Step 5 of 6
+// Event Handling with If & Else Statements
 
 var button = document.querySelector(".show-cat");
-// console.log(button);
+console.log(button);
 
 var cat = document.querySelector("figure.cat");
 // console.log(cat);
 
 button.addEventListener("click", function () {
-    cat.classList.add("show");
+    if (cat.classList.contains("show")) {
+        cat.classList.remove("show");
+        button.innerText = "Wait, come back!";
+        button.classList.add("disappear");
+    } else {
+        cat.classList.add("show");
+        button.innerText = "Shoo, cat!";
+        button.classList.remove("disappear");
+    }
 })
