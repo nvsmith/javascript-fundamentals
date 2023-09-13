6186ed0fee2df5b0125d48450e1e1d3404d67396
Nathan Smith <nathanielvs@gmail.com>
2023-09-12 23:27:52 -0600

    Bonus: open modal function, cleanup code comments and layout

--- module-07-keydown-change-events/function-powered-modal/js/script.js
+++ module-07-keydown-change-events/function-powered-modal/js/script.js
@@ -5,9 +5,20 @@ var button = document.querySelector(".got-this");
 var modal = document.querySelector(".modal");
 var modalX = document.querySelector(".modal-x");
 
-button.addEventListener("click", function () {
+// Close and Open Modal Functions
+var closeModal = function () {
+  modal.classList.remove("show-modal");
+  button.innerText = "Who's got this?";
+};
+
+var openModal = function () {
   modal.classList.add("show-modal");
   button.innerText = "You've got this!!!";
+};
+
+// Esc and Click Events
+button.addEventListener("click", function () {
+  openModal();
 });
 
 modalX.addEventListener("click", function () {
@@ -21,9 +32,4 @@ document.addEventListener("keydown", function (e) {
       closeModal();
     }
   }
-})
-
-var closeModal = function () {
-  modal.classList.remove("show-modal");
-  button.innerText = "Who's got this?";
-}
+});
