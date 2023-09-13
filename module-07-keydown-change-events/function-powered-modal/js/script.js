10123c576bf24749b7f45a8dca8b5abe25b433b5
Nathan Smith <nathanielvs@gmail.com>
2023-09-12 23:12:45 -0600

    Close modal with Esc keydown function

---
+++ module-07-keydown-change-events/function-powered-modal/js/script.js
@@ -0,0 +1,27 @@
+// Step 4 of 6
+// Function Powered Modal
+
+var button = document.querySelector(".got-this");
+var modal = document.querySelector(".modal");
+var modalX = document.querySelector(".modal-x");
+
+button.addEventListener("click", function () {
+  modal.classList.add("show-modal");
+  button.innerText = "You've got this!!!";
+});
+
+modalX.addEventListener("click", function () {
+  modal.classList.remove("show-modal");
+  button.innerText = "Who's got this?";
+});
+
+document.addEventListener("keydown", function (e) {
+  // console.log(e.key);
+  if (e.key === "Escape") {
+    // console.log(e.key);
+    if (modal.classList.contains("show-modal")) {
+      modal.classList.remove("show-modal");
+      button.innerText = "Who's got this?";
+    }
+  }
+})
