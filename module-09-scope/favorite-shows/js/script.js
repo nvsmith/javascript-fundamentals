ea7c86829aa3743d981f0b63f38e0c01f2c13cbd
Nathan Smith <nathanielvs@gmail.com>
2023-09-23 21:19:16 -0600

    Get user input & populate on Webpage

---
+++ module-09-scope/favorite-shows/js/script.js
@@ -0,0 +1,25 @@
+// Step 2 of 6
+// Scope, Input, & Creating Elements
+
+// Get Favorite TV Shows Input from User & Populate on Webpage
+
+// add button
+const addShowButton = document.querySelector(".add");
+// label for the add button
+const showInput = document.querySelector(".add-show input");
+// unordered list (not yet visible)
+const showList = document.querySelector(".show-list");
+// span class for number of favorite shows
+const showCount = document.querySelector(".number");
+
+addShowButton.addEventListener("click", function () {
+    const show = showInput.value;
+    if (show !== "") {
+        let listItem = document.createElement("li");
+        listItem.innerText = show;
+        showList.append(listItem);
+        let shows = document.querySelectorAll(".show-list li");
+        showCount.innerText = shows.length;
+    }
+});
+
