804dd26c14c8cdb0ec16ae64af6aca60cb7d7992
Nathan Smith <nathanielvs@gmail.com>
2023-09-25 09:38:32 -0600

    Refactor: create separate function for adding guests to lists

--- module-09-scope/potluck-guest-list/js/script.js
+++ module-09-scope/potluck-guest-list/js/script.js
@@ -18,13 +18,17 @@ const guestFull = document.querySelector(".alert");
 addGuestButton.addEventListener("click", function () {
    const guest = guestInput.value;
    if (guest !== "") {
-    let listItem = document.createElement("li");
-    listItem.innerText = guest;
-    guestList.append(listItem);
+    addToList(guest);
     clearInput();
    }   
 });
 
 const clearInput = function () {
     guestInput.value = ""; 
+};
+
+const addToList = function (guest) {
+    const listItem = document.createElement("li");
+    listItem.innerText = guest;
+    guestList.append(listItem);
 };
