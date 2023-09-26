002b29c4cebccfed81033a943c0ef1ab9bd5b02f
Nathan Smith <nathanielvs@gmail.com>
2023-09-26 09:43:05 -0600

    Assignment functionality: list guest & item, prevent duplicates, disable button upon completion

--- module-09-scope/potluck-guest-list/js/script.js
+++ module-09-scope/potluck-guest-list/js/script.js
@@ -14,7 +14,12 @@ const guestCount = document.querySelector(".attendance");
 // alert when guest list is full (not yet visible)
 const guestFull = document.querySelector(".alert");
 
-// On click, add input to guest list
+// appears when guest list is full (not yet visible)
+const assignButton = document.querySelector(".assign");
+// list with guest name and dish assignment (not yet visible)
+const assignedItems = document.querySelector(".assigned-items");
+
+// On click, add user input to guest list
 addGuestButton.addEventListener("click", function () {
    const guest = guestInput.value;
    if (guest !== "") {
@@ -24,19 +29,20 @@ addGuestButton.addEventListener("click", function () {
    }   
 });
 
-const clearInput = function () {
-    guestInput.value = ""; 
-};
-
 const addToList = function (guest) {
     const listItem = document.createElement("li");
     listItem.innerText = guest;
     guestList.append(listItem);
 };
 
+const clearInput = function () {
+    guestInput.value = ""; 
+};
+
 updateGuestCount = function () {
     const guests = document.querySelectorAll(".guest-list li");
     guestCount.innerText = guests.length;
+
     if (guests.length === 8) {
         addGuestButton.classList.add("hide");
         guestInput.classList.add("hide");
@@ -44,3 +50,39 @@ updateGuestCount = function () {
         guestFull.classList.remove("hide");
     }
 };
+
+const assignItems = function () {
+    const potluckItems = [
+        "Bruschetta",
+        "Nori Rolls",
+        "Caprese Skewers",
+        "Stuffed Peppadews",
+        "Couscous Salad",
+        "Icebox Cake",
+        "Pasta Salad",
+        "Ribs",
+        "Buffalo Chicken",
+        "Cheese Board"
+    ];
+
+    const allGuests = document.querySelectorAll(".guest-list li");
+    
+    for (let guest of allGuests) {
+        let randomPotluckIndex = Math.floor(Math.random() * potluckItems.length);
+        let randomPotluckItem = potluckItems[randomPotluckIndex];
+        let listItem = document.createElement("li");
+        
+        /* Use guest.innerText to get name inside the li. 
+        Otherwise you’d get the li element itself */
+        listItem.innerText = `${guest.innerText} is bringing ${randomPotluckItem}`;
+        assignedItems.append(listItem);
+        
+        /* Prevent duplicate assignments */
+        potluckItems.splice(randomPotluckIndex, 1);
+    }
+};
+
+assignButton.addEventListener("click", function () {
+    assignItems();
+    assignButton.disabled = true;
+});
