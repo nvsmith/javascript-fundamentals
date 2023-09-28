8a29e4b8f9d44a3844db7f50d8c8c0153992fbd0
Nathan Smith <nathanielvs@gmail.com>
2023-09-28 14:38:19 -0600

    Object dot notation

---
+++ module-10-objects-methods/objects1/js/script.js
@@ -0,0 +1,28 @@
+// Step 4 of 6
+// Properties & Dot Notation
+
+const topicElement = document.querySelector(".topic");
+const countElement = document.querySelector(".count");
+const learning = {};
+    
+learning.topic = "JS";
+learning.learningGoals = [
+        "Build JS Programs", 
+        "Build Mini Apps",
+        "Animate Cool Effects",
+        "Make A Game"
+    ];
+learning.category = "Front End Development";
+learning.topicImportance = "High";
+
+console.log(learning);
+
+learning.topic = "JavaScript";
+learning.learningGoals.splice(1,1);
+console.log(learning);
+
+topicElement.innerText = `✔️ I'm learning ${learning.topic}.`;
+topicElement.classList.remove("hide");
+
+countElement.innerText = `✔️ I have ${learning.learningGoals.length} learning goals.`;
+countElement.classList.remove("hide");
