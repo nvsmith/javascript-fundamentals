0649f8da6e47bdfdba2376f06c239f3e715df40e
Nathan Smith <nathanielvs@gmail.com>
2023-10-02 15:06:41 -0600

    Display pet objects on webpage: for...of loop/array, create/append element

--- module-11-factory-functions/pet-rescue-adoption/js/script.js
+++ module-11-factory-functions/pet-rescue-adoption/js/script.js
@@ -6,6 +6,11 @@
 // name, species, and tiredness as parameters.
 
 
+// Step 5 of 6
+// Display Pet Objects
+
+// Use pet data to populate the webpage. Take the pet objects you created
+// and place them in an array. Loop over all objects at once using the for…of loop.
 
 
 const statusButton = document.querySelector("button");
@@ -41,6 +46,34 @@ const baxter = createPet("Baxter", "hamster");
 const cleo = createPet("Cleo", "rat");
 const francine = createPet("Francine", "turtle");
 
-clover.sleep();
-baxter.play();
-console.log(sora, clover, baxter);
+// clover.sleep();
+// baxter.play();
+// console.log(sora, clover, baxter);
+
+clover.isTired = 8;
+francine.isTired = 9;
+
+// Create array of our pet objects
+allPets = [sora, clover, baxter, cleo, francine];
+console.log(allPets);
+
+// Display pets in the browser
+const showPets = function (petArray) {
+    // empty list
+    pets.innerHTML = "";
+
+    for(let pet of petArray) {
+        let status = "ready to play";
+        if (pet.isTired >= 7) {
+            status = "is sleeping";
+        }
+
+        const li = document.createElement("li");
+        li.innerHTML = `<span class="pet-name">${pet.name}</span> the ${pet.species} is ${status}`;
+        pets.append(li);
+    }
+};
+
+statusButton.addEventListener("click", function () {
+   showPets(allPets); 
+});
