dff428dd72fbc921c03239b677d508abbb5b55e8
Nathan Smith <nathanielvs@gmail.com>
2023-10-02 21:09:37 -0600

    Vehicle2: display vehicles on click that need repairs: arrays, for...of, filter, create/append list items

--- module-11-factory-functions/weekend-repair-project/js/script.js
+++ module-11-factory-functions/weekend-repair-project/js/script.js
@@ -1,10 +1,18 @@
 // Step 6 of 6
 // Practice Exercises: Factory Functions
 
+
+
 // Exercise #1
-// factory function, object literals, add objects, array, for…of loop, for…in loop
+// factory function, object literals, add objects, 
+// array, for…of loop, for…in loop
 
+// Exercise #2
+// add objects, empty array, for…of loop, if statement, 
+// push(), innerHTML, createElement, append()
 
+const button = document.querySelector("button");
+const repairList = document.querySelector(".need-repair");
 
 createVehicle = function (type, numwheels, color){
     const vehicle = {
@@ -26,3 +34,23 @@ for (let vehicle of myVehicles) {
         console.log(`${key}: ${vehicle[key]}`);
     }
 }; 
+
+bike.needsRepair = true;
+skateboard.needsRepair = true;
+
+// Create an event that will display the vehicles that need repairing when a button is clicked.
+button.addEventListener("click", function () {
+  repairList.innerHTML = ""; // Clear the list before populating it again
+
+  // Filter the vehicles that need repairs from all other vehicles
+  const vehicleRepairList = myVehicles.filter(function (vehicle) {
+    return vehicle.needsRepair === true;
+  });
+  
+  // Loop thru each vehicle needing repairs and append it to the webpage
+  for (let vehicle of vehicleRepairList) {
+    let li = document.createElement("li");
+    li.innerHTML = `My <span>${vehicle.type}</span> needs some attention.`;
+    repairList.append(li);
+  }
+});
