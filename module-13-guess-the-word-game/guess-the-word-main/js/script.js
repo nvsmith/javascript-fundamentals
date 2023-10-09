6df1ba3f0ce4925318243694354c03413289122c
Nathan Smith <nathanielvs@gmail.com>
2023-10-09 15:41:42 -0600

    Add placeholders for word letters and click event for guess button.

--- module-13-guess-the-word-game/guess-the-word-main/js/script.js
+++ module-13-guess-the-word-game/guess-the-word-main/js/script.js
@@ -4,18 +4,42 @@
 // The unordered list where guessed letters will appear.
 const guessedLetters = document.querySelector(".guessed-letters");
 // The "Guess!" button.
-const guess = document.querySelector(".guess");
+const guessLetterButton = document.querySelector(".guess");
 // The text input where the player will guess a letter.
-const letter = document.querySelector("#letter");
+const letterInput = document.querySelector("#letter");
 // The empty paragraph where the word in progress will appear.
 const wordInProgress = document.querySelector(".word-in-progress");
 // The paragraph where the remaining guesses will display.
-const remaining = document.querySelector(".remaining");
+const remainingGuessesElement = document.querySelector(".remaining");
 // The span where the remaining guesses will display.
-const span = document.querySelector(".remaining span");
+const remainingGuessesSpan = document.querySelector(".remaining span");
 // The empty paragraph where messages will appear when the player guesses a letter.
 const message = document.querySelector(".message");
 // The hidden "Play Again!" button.
-const playAgain = document.querySelector(".play-again");
+const playAgainButton = document.querySelector(".play-again");
+
 // Start word for testing.
 const word = "magnolia";
+
+// Display placeholder symbols for the word's letters
+const placeholder = function (word) {
+    // Create an empty array to hold the placeholder symobls
+    placeholderLetters = [];
+    // For each letter of the word, add a placeholder into the array.
+    for (const letter of word) {
+        placeholderLetters.push("●");
+    }
+    // Add the array to the document.
+    wordInProgress.innerText = placeholderLetters.join("");
+};
+
+placeholder(word);
+
+guessLetterButton.addEventListener("click", function (e) {
+    // Prevent form submission and reloading the page.
+    e.preventDefault();
+    const guess = letterInput.value;
+    console.log(guess);
+    // Clear the input after  guessing.
+    letter.value = "";
+});
