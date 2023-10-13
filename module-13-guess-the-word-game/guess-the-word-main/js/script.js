098adaf7f87f9985989c658c4082b1c90886d5e9
Nathan Smith <nathanielvs@gmail.com>
2023-10-12 20:55:54 -0600

    Check if the player guessed the word and update message

--- module-13-guess-the-word-game/guess-the-word-main/js/script.js
+++ module-13-guess-the-word-game/guess-the-word-main/js/script.js
@@ -117,10 +117,15 @@ const updateWordInProgress = function (guessedLetters) {
 
     // Update the display with the revealed characters.
     wordInProgress.innerText = revealWord.join("");
+
+    checkIfWin();
 };
 
 // Check if player has guessed the word
-const checkWin = function () {
-    if (condition) {
+const checkIfWin = function () {
+    if (word.toUpperCase() === wordInProgress.innerText) {
+        message.classList.add("win");
+        message.innerHTML = `<p class="highlight">
+        You guessed correct the word! Congrats!</p>`;
     }
 };
